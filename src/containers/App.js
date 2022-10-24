8400fe2ffeee29a566134f5ff4ee8dd3fa6e4316
Nguyen <babyfacedeveloper@gmail.com>
2022-10-24 18:38:54 -0400

    apply redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,44 +1,61 @@
-import React, { useState } from "react";
+import React from "react";
+import { connect } from 'react-redux';
 import CardList from "../components/cardList";
 import SearchBox from "../components/searchBox";
 import './App.css';
 import Scroll from "../components/scroll";
 import ErrorBoundry from "../components/errorBoundry";
-import { useEffect } from "react";
+import { setSearchfield } from '../actions';
 
-const App = () => {
-    const [robots, setRobots] = useState([]);
-    const [searchfield, setSearchfield] = useState('');
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
 
-    const onSearchChange = (event) => {
-        setSearchfield(event.target.value);
+const mapDispatchToProps = dispatch => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchfield(event.target.value))
+    }
+}
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: []
+        };
     }
 
-    useEffect(() => {
+    componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => {
-                setRobots(users);
+                this.setState({ robots: users });
             })
-    }, []);
+    }
 
-    const filteredRobots = robots.filter(robot => {
-        return robot.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase());
-    });
+    render() {
+        const { robots } = this.state;
+        const { searchField, onSearchChange } = this.props;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLocaleLowerCase().includes(searchField.toLocaleLowerCase());
+        });
 
-    return !robots.length ?
-        <h1>Loading</h1> :
-        (
-            <div className="tc" >
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={onSearchChange} />
-                <Scroll>
-                    <ErrorBoundry>
-                        <CardList robots={filteredRobots} />
-                    </ErrorBoundry>
-                </Scroll>
-            </div>
-        );
+        return !robots.length ?
+            <h1>Loading</h1> :
+            (
+                <div className="tc" >
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={onSearchChange} />
+                    <Scroll>
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
+                    </Scroll>
+                </div>
+            );
+    }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './containers/App';
 import 'tachyons';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
